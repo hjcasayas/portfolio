bc1106db4200afa91cddf7b4802cd6e1f55677b6
hjcasayas <henlyjade.casayas@gmail.com>
2018-10-26 02:43:26 +0800

    finished styling mobile header menu icon

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ var path = require('path');
 
 module.exports = {
 	entry: {
-		App: "./app/assets/scripts/App.js",
-		Vendor: "./app/assets/scripts/Vendor.js"	
+		App: "./app/assets/scripts/App.js"
+		// Vendor: "./app/assets/scripts/Vendor.js"	
 	},
 	output: {
 		path: path.resolve(__dirname, "./app/temp/scripts"),
